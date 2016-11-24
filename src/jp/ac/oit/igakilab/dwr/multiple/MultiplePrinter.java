1da029c6a63bee8c9497971db39a992609507b75
kitaba-marshmallow <e1b13038@gmail.com>
2016-11-24 15:35:07 +0900

    コミット

--- src/jp/ac/oit/igakilab/dwr/multiple/MultiplePrinter.java
+++ src/jp/ac/oit/igakilab/dwr/multiple/MultiplePrinter.java
@@ -2,15 +2,16 @@
 package jp.ac.oit.igakilab.dwr.multiple;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
 import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
 
 /**
  * DWRでJSから呼ばれるメソッドはすべてpublicでなければならない．また，必要なクラスはすべてdwr.xmlに定義されている必要がある．
@@ -18,24 +19,61 @@ import com.mongodb.client.model.Filters;
  *
  */
 public class MultiplePrinter {
-	public List<String> execute(MultipleForm input) throws InvalidValueException {
-	    List<String> list = new ArrayList<>();
+	public List<Mozyao> execute() throws InvalidValueException {
+	    List<Mozyao> list = new ArrayList<>();
+	    List<Integer> minors = Arrays.asList(101, 102, 103);
 
-	    List<BeaconPos> bpos = getBeaconPositions(102);
-	    for(int i=0;i<bpos.size(); i++){
-	    	BeaconPos p = bpos.get(i);
-	    	String str = "x = " + p.getX() + ", y = " + p.getY();
-	    	list.add(str);
+	    for(int i=0; i<minors.size(); i++){
+	    	//最新の座標をpに取ってくる
+	    	BeaconPos p = getBeaconPositions(minors.get(i));
+
+	    	//Mozyaoに値を設定する
+	    	Mozyao m = new Mozyao();
+	    	m.minor = minors.get(i);
+	    	if( p != null ){
+	    		m.x = p.getX();
+	    		m.y = p.getY();
+	    	}
+
+	    	//返却するリストに入れる
+	    	list.add(m);
 	    }
+
 	    return list;
 	}
+
+	public static class Mozyao{
+		private int minor;
+		private double x,y;
+
+		public int getMinor() {
+			return minor;
+		}
+		public void setMinor(int minor) {
+			this.minor = minor;
+		}
+		public double getX() {
+			return x;
+		}
+		public void setX(double x) {
+			this.x = x;
+		}
+		public double getY() {
+			return y;
+		}
+		public void setY(double y) {
+			this.y = y;
+		}
+
+	}
+
 
 	/**
 	 * ビーコンの座標の一覧を取得します
 	 * @param beaconId 対象のビーコンIDです
 	 * @return
 	 */
-	private List<BeaconPos> getBeaconPositions(int beaconId){
+	private BeaconPos getBeaconPositions(int beaconId){
 		//クライアントの作成
 		MongoClient client = new MongoClient("150.89.234.253");
 		MongoCollection<Document> col = client
@@ -45,22 +83,20 @@ public class MultiplePrinter {
 		Bson filter = Filters.eq("minor", beaconId);
 
 		//結果の取得
-		FindIterable<Document> result = col.find(filter);
-		List<BeaconPos> positions = new ArrayList<BeaconPos>();
+		Document doc = col.find(filter).sort(Sorts.descending("date")).first();
 
-		//変換
-		for(Document doc : result){
-			BeaconPos pos = new BeaconPos();
-			pos.setDate(doc.getDate("date"));
-			pos.setX(doc.getDouble("x座標"));
-			pos.setY(doc.getDouble("y座標"));
-			positions.add(pos);
-			if( positions.size() > 3 ){
-				positions.remove(0);
-			}
+		if( doc == null ){
+			client.close();
+			return null;
 		}
 
+		//変換
+		BeaconPos pos = new BeaconPos();
+		pos.setDate(doc.getDate("date"));
+		pos.setX(doc.getDouble("x座標"));
+		pos.setY(doc.getDouble("y座標"));
+
 		client.close();
-		return positions;
+		return pos;
 	}
 }
